86958e20f2f72c41dcf54779c90346f9295517b4
Nicolas Fraison <nfraison@yahoo.fr>
2024-01-13 00:00:53 +0100

    Fix syntax

--- nixos/users-configuration.nix
+++ nixos/users-configuration.nix
@@ -1,19 +1,24 @@
 {
-  # Groups
-  users.groups.family = {};
+  users = {
+    # Groups
+    groups.family = {};
 
-  # Users
-  users.users.nfraison = {
-    isNormalUser = true;
-    description = "Nicolas Fraison";
-    home = "/home/nfraison";
-    extraGroups = [ "networkmanager" "wheel" "family" "docker" "scanner" "lp" ];
-  };
-  
-  users.users.mherson = {
-    isNormalUser = true;
-    description = "Magali Herson";
-    home = "/home/mherson";
-    extraGroups = [ "networkmanager" "wheel" "family" "scanner" "lp" ];
+
+    # Users
+    users = {
+      nfraison = {
+        isNormalUser = true;
+        description = "Nicolas Fraison";
+        home = "/home/nfraison";
+        extraGroups = [ "networkmanager" "wheel" "family" "docker" "scanner" "lp" ];
+      };
+
+      mherson = {
+        isNormalUser = true;
+        description = "Magali Herson";
+        home = "/home/mherson";
+        extraGroups = [ "networkmanager" "wheel" "family" "scanner" "lp" ];
+      };
+    };
   };
 }
